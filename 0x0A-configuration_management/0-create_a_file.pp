9313d4e0607ac1e20fed20e3c8a893d51e4faa72
Fatimazahraker <fatimazahraker>
2023-09-22 21:06:15 +0000

    task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a file in /tmp.
 file { '/tmp/school':
-  owner      => 'www-data',
-  group      => 'www-data',
-  mode => '0744',
-  content    => "I love Puppet"
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
 }
